5ec8ee58a0101d5acda8dc1962709a458cd8464c
Azalurg <91213389+Azalurg@users.noreply.github.com>
2023-01-06 16:29:21 +0100

    Create simple express app

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-console.log("Hello there")
+import express, { Request, Response } from "express";
+const app = express();
+
+const PORT = 3000
+
+app.get("/", (req: Request, res: Response): void => {
+  res.json({ message: "Hello there" });
+});
+
+app.listen(PORT, (): void => {
+  console.log(`Server Running on port ${PORT}`);
+});
